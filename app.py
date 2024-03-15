fe8c4c26db8ff7e0fda4b7ca9aea6df84013911a
lucas marcondes <lucas.marcondes@zigpay.com.br>
2024-03-15 10:06:32 -0300

    # Changes to be committed: #	modified:   app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
-import pandas as pd
 import requests
 from urllib.parse import urlencode
+import pandas as pd
 
+# Função para geolocalizar endereço usando a API do OpenStreetMap
 def geolocalizar(endereco):
     base_url = "https://nominatim.openstreetmap.org/search?"
     parametros = {'q': endereco, 'format': 'json'}
@@ -14,14 +15,44 @@ def geolocalizar(endereco):
         return dados['lat'], dados['lon']
     return None, None
 
+# Função principal
 def main():
-    st.title("Geolocalização de Endereços")
+    st.title("Geolook")
     
-    uploaded_file = st.file_uploader("Carregar arquivo CSV contendo os endereços", type="csv")
-    if uploaded_file is not None:
-        enderecos_df = pd.read_csv(uploaded_file)
-        enderecos_df['Latitude'], enderecos_df['Longitude'] = zip(*enderecos_df['Endereço'].apply(geolocalizar))
-        st.write(enderecos_df)
+    # Campo de texto (text area) para inserir os endereços
+    enderecos_input = st.text_area("Insira os endereços (um por linha):", height=200)
+
+    # Botão para localizar coordenadas
+    if st.button("Localizar"):
+        if enderecos_input:
+            # Dividir os endereços em uma lista
+            enderecos = enderecos_input.split("\n")
+            
+            # Lista para armazenar os resultados
+            resultados = []
+            
+            # Iterar sobre cada endereço e localizar as coordenadas
+            for endereco in enderecos:
+                if endereco.strip():
+                    lat, lng = geolocalizar(endereco.strip())
+                    if lat and lng:
+                        resultados.append({'Endereço': endereco.strip(), 'Latitude': lat, 'Longitude': lng})
+                    else:
+                        st.error(f"Não foi possível encontrar as coordenadas para o endereço: {endereco.strip()}")
+            
+            # Criar DataFrame com os resultados
+            df_resultados = pd.DataFrame(resultados)
+            
+            # Exibir DataFrame na interface
+            st.write("Resultados:")
+            st.write(df_resultados)
+            
+            # Botão para baixar os resultados em CSV
+            csv = df_resultados.to_csv(index=False).encode('utf-8-sig')
+            st.download_button(label="Baixar Resultados em CSV", data=csv, file_name='resultados_geolocalizacao.csv', mime='text/csv')
+            
+        else:
+            st.warning("Por favor, insira pelo menos um endereço para realizar a busca.")
 
 if __name__ == "__main__":
     main()
